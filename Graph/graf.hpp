ae792ad8ae5d95ccf1342c81a29356718e6ff6ff
JJWasyl <37805855+JJWasyl@users.noreply.github.com>
2018-05-27 19:42:20 +0200

    Add files via upload

--- Graph/graf.hpp
+++ Graph/graf.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <vector>
 #include <stdexcept>
@@ -11,16 +12,16 @@ class Graph{
     public:
     class Vertex{
     public:
-        
+
         Vertex(){
             this->index = -1;
         }
-        
+
         Vertex(const V &val, const int &ind){
             this->value = val;
             this->index = ind;
 
-        }    
+        }
         V value;
         int index;
     };
@@ -28,15 +29,15 @@ class Graph{
     public:
     //edge info
     class Edge{
-    public:        
+    public:
         Edge(){
             this->exists = false;
         }
-        
+
         Edge(E label){
             this->label = label;
             this->exists = true;
-        }       
+        }
         bool exists;
         E label;
     };
@@ -46,14 +47,14 @@ class Graph{
         vector<Vertex> verx;
         int edgecount;
         int vertexIndex;
-        
+
     public:
         Graph();
         Graph(const Graph &source);
         Graph(Graph &&source);
         Graph& operator=(const Graph &source);
         Graph& operator=(Graph &&source) = default;
-    
+
     public:
         class EdgeIterator{
             friend class Graph;
@@ -61,18 +62,18 @@ class Graph{
                 int it_row, it_col;
                 Graph<V,E>* source;
                 EdgeIterator(Graph<V,E> &graph, int row, int col);
-                
+
             public:
-                bool operator==(const EdgeIterator &ei) const;                
-                bool operator!=(const EdgeIterator &ei) const{return !(*this == ei);}                
-                EdgeIterator& operator++();                
-                EdgeIterator operator++(int); 
+                bool operator==(const EdgeIterator &ei) const;
+                bool operator!=(const EdgeIterator &ei) const{return !(*this == ei);}
+                EdgeIterator& operator++();
+                EdgeIterator operator++(int);
                 E& operator*();
         };
-        EdgeIterator beginEdge();
-        EdgeIterator endEdge();
-        
-    public:      
+        EdgeIterator beginEdges();
+        EdgeIterator endEdges();
+
+    public:
         class VertexIterator{
             friend class Graph;
             private:
@@ -85,10 +86,11 @@ class Graph{
                 VertexIterator& operator++();
                 VertexIterator operator++(int);
                 V& operator*();
+                int& getIndex();
         };
-        VertexIterator beginVertex();
-        VertexIterator endVertex();
-    
+        VertexIterator beginVertices();
+        VertexIterator endVertices();
+
     public:
         class DFS{
             friend class Graph;
@@ -98,18 +100,18 @@ class Graph{
                 stack<int> path;
                 int current_vertex_id;
                 DFS(Graph<V,E> &graph, int current_vertex = 0);
-                
+
             public:
                 bool operator==(const DFS &dfs) const;
                 bool operator!=(const DFS &dfs) const{return !(*this == dfs);}
                 DFS& operator++();
                 DFS operator++(int);
                 V& operator*();
-        };        
-        DFS beginDFS();
+        };
+        DFS beginDFS(int init=0);
         DFS endDFS();
-    
-    public:    
+
+    public:
         class BFS{
             friend class Graph;
             private:
@@ -118,26 +120,26 @@ class Graph{
                 list<int> queue;
                 int current_vertex_id;
                 BFS(Graph<V,E> &graph, int current_vertex = 0);
-            
+
             public:
                 bool operator==(const BFS &bfs) const;
                 bool operator!=(const BFS &bfs) const{return !(*this == bfs);}
                 BFS& operator++();
                 BFS operator++(int);
                 V& operator*();
-        };        
-        BFS beginBFS();
+        };
+        BFS beginBFS(int init=0);
         BFS endBFS();
-        
+
     public:
-        bool addEdge(int orig, int dest, const E &label = E{}, bool replace = true);
-        bool addVertex(const V &val);
+        bool insertEdge(int orig, int dest, const E &label = E{}, bool replace = true);
+        bool insertVertex(const V &val);
         bool removeVertex(int vertIndex);
         bool removeEdge(int index1, int index2);
         bool edgeExists(int index1, int index2) const;
-        int noVertices() const;
-        int noEdges() const;
-        void printNM();
+        int nrOfVertices() const;
+        int nrOfEdges() const;
+        void printNeighborhoodMatrix();
         V& vertexData(int vertex_id);
         E& edgeLabel(int origin, int dest);
 
@@ -158,14 +160,14 @@ Graph<V,E>::Graph(const Graph<V,E> &source){
     dirmat = source.dirmat;
     verx = source.verx;
     edgecount = source.edgecount;
-    vertexIndex = source.vertexIndex;    
+    vertexIndex = source.vertexIndex;
 }
 
 template <typename V, typename E>
 Graph<V,E>& Graph<V,E>::operator=(const Graph<V,E> &source){
     if(this == &source){
         return *this;
-    }    
+    }
     dirmat = source.dirmat;
     verx = source.verx;
     edgecount = source.edgecount;
@@ -187,7 +189,7 @@ template <typename V, typename E>
 Graph<V,E>& operator=(Graph<V,E> &&source){
     if(this == &source){
         return *this;
-    }    
+    }
     dirmat = source.dirmat;
     verx = source.verx;
     edgecount = source.edgecount;
@@ -201,7 +203,7 @@ Graph<V,E>& operator=(Graph<V,E> &&source){
  */
 
 template <typename V, typename E>
-bool Graph<V,E>::addEdge(int orig, int dest, const E &label, bool replace){
+bool Graph<V,E>::insertEdge(int orig, int dest, const E &label, bool replace){
     if(edgeExists(orig, dest)){ //rozszerzyć krawędź
         if(replace == true){
             dirmat[orig][dest] = Edge(label);
@@ -210,14 +212,13 @@ bool Graph<V,E>::addEdge(int orig, int dest, const E &label, bool replace){
         }
     } else {
         dirmat[orig][dest] = Edge(label);
-        
+        edgecount++;
     }
-    edgecount++;
     return true;
 }
 
 template <typename V, typename E>
-bool Graph<V,E>::addVertex(const V &val){
+bool Graph<V,E>::insertVertex(const V &val){
     int size = verx.size();
     verx.push_back(Vertex(val, vertexIndex));
     vertexIndex++;
@@ -240,27 +241,31 @@ bool Graph<V,E>::addVertex(const V &val){
 //Commented methods used to reduce the Vertex vector
 template <typename V, typename E>
 bool Graph<V,E>::removeVertex(int vertIndex){
-    int size = verx.size();
-    if(size <= 0)
+    if(verx.size() <= 0)
         return false;
-    for(int k = 0; k < size; k++){
-        if(verx[k].index == vertIndex){
-            /*
-            for(int i = size; i > vertIndex; i--){
-                verx[i].index--;
-            }
-            */
-            size--;
-            //verx.erase(verx.begin() + k);
-            verx[k] = Vertex();
-            dirmat.erase(dirmat.begin() + vertIndex);
-            for(int k = 0; k < size; k++){
-                dirmat[k].erase(dirmat[k].begin() + vertIndex);
-            }
-            break;
+    verx.erase(verx.begin() + vertIndex);
+    vertexIndex--;
+    
+    //Correct the internal indexing order
+    for(int z = vertIndex; z < verx.size(); z++){
+        verx[z].index--;
+    }
+    
+    //Correct edgecount and remove row
+    for(int i = 0; i < dirmat[vertIndex].size(); i++){
+        if(dirmat[vertIndex][i].exists){
+            edgecount--;
         }
     }
-    vertexIndex--;
+    dirmat.erase(dirmat.begin() + vertIndex);
+    
+    //Remove column and correct endgecount
+    for(int k = 0; k < dirmat[0].size(); k++){
+        if(dirmat[k][vertIndex].exists){
+            edgecount--;
+        }
+        dirmat[k].erase(dirmat[k].begin() + vertIndex);
+    }
     return true;
 }
 
@@ -269,7 +274,7 @@ bool Graph<V,E>::removeEdge(int index1, int index2){
     int size = dirmat[0].size();
     if(index1 > size || index2 > size)
         return false;
-    
+
     if(edgeExists(index1, index2)){
         dirmat[index1][index2] = Edge();
         edgecount--;
@@ -280,47 +285,57 @@ bool Graph<V,E>::removeEdge(int index1, int index2){
 
 template <typename V, typename E>
 bool Graph<V,E>::edgeExists(int index1, int index2) const{
+    if(index1 > vertexIndex || index2 > vertexIndex ||
+            index1 < 0 || index2 < 0)
+        return false;
     return dirmat[index1][index2].exists;
 }
 
 template <typename V, typename E>
-int Graph<V,E>::noVertices() const{
-    int temp = 0;
-    for(int k = 0; k < int(verx.size()); k++){
-        if(verx[k].index != -1)
-            temp++;
-    }
-    return temp;
+E& Graph<V,E>::edgeLabel(int origin, int dest){
+    if(origin > vertexIndex || dest > vertexIndex ||
+            origin < 0 || dest < 0)
+        throw invalid_argument("indexes out of range");
+    return dirmat[origin][dest].label;
+}
+
+template <typename V, typename E>
+V& Graph<V,E>::vertexData(int vertex_id){
+    if(vertex_id > verx.size() || vertex_id < 0)
+        throw invalid_argument("Vertex does not exist");
+    else
+        return verx[vertex_id].value;
+}
+
+template <typename V, typename E>
+int Graph<V,E>::nrOfVertices() const{
+    return verx.size();
 }
 
 //Sprawdzic
 template <typename V, typename E>
-int Graph<V,E>::noEdges() const{
-    return edgecount;      
+int Graph<V,E>::nrOfEdges() const{
+    return edgecount;
 }
 
 //Multiple if statements to account for removed vertices still present as empty
 //slots in the verx vector
 //Technically they do nothing if the matrix was not reduced
 template <typename V, typename E>
-void Graph<V,E>::printNM(){
+void Graph<V,E>::printNeighborhoodMatrix(){
     int size = verx.size();
     cout << "__|_";
     for(int z = 0; z < size; z++){
-        if(verx[z].index != -1)
-            cout << verx[z].index << "_"; 
+        cout << verx[z].index << "_";
     }
     cout << endl;
     for(int k = 0; k < size; k++){
-        if(verx[k].index != -1)
-            cout << verx[k].index << " | ";
+        cout << verx[k].index << " | ";
         for(int i = 0; i < size; i++){
-            if(verx[k].index != -1 && verx[i].index != -1)
-                cout << dirmat[k][i].exists << " ";
+            cout << dirmat[k][i].exists << " ";
         }
-        if(verx[k].index != -1)
-            cout << endl;
-    }   
+    cout << endl;
+    }
 }
 
 /*
@@ -335,14 +350,14 @@ Graph<V,E>::EdgeIterator::EdgeIterator(Graph<V,E> &graph, int row, int col){
         source = nullptr;
         it_row = -1;
         it_col = -1;
-    } else if(row > graph.edgecount || col > graph.edgecount){
+    } else if(row > graph.nrOfVertices() || col > graph.nrOfVertices()){
         throw invalid_argument("Values outside of graph");
     } else {
         it_row = row; it_col = col;
-        while(graph.dirmat[it_row][it_col].exists != 1){
-            if(it_col == graph.edgecount && it_row == graph.edgecount){
+        while(graph.dirmat[it_row][it_col].exists != true){
+            if(it_col == graph.nrOfVertices() && it_row == graph.nrOfVertices()){
                 throw invalid_argument("No edges detected");
-            } else if(it_col == graph.edgecount){
+            } else if(it_col == graph.nrOfVertices()){
                 it_col = 0;
                 it_row++;
             } else {
@@ -361,18 +376,18 @@ bool Graph<V,E>::EdgeIterator::operator==(const EdgeIterator &ei) const{
 template <typename V, typename E>
 typename Graph<V,E>::EdgeIterator& Graph<V,E>::EdgeIterator::operator++(){
     do{
-        if(it_col == source->edgecount && it_row == source->edgecount){
+        if(it_col == source->nrOfVertices() && it_row == source->nrOfVertices()){
             it_col = -1;
             it_row = -1;
             source = nullptr;
             return (*this);
-        } else if(it_col == source->edgecount){
+        } else if(it_col == source->nrOfVertices()){
             it_col = 0;
             it_row++;
         } else {
             it_col++;
         }
-    }while(source->dirmat[it_row][it_col].exists != 1);
+    } while(source->dirmat[it_row][it_col].exists != 1);
     return(*this);
 }
 
@@ -389,12 +404,12 @@ E& Graph<V,E>::EdgeIterator::operator*(){
 }
 
 template <typename V, typename E>
-typename Graph<V,E>::EdgeIterator Graph<V,E>::beginEdge(){
+typename Graph<V,E>::EdgeIterator Graph<V,E>::beginEdges(){
     return(EdgeIterator(*this, 0, 0));
 }
 
 template <typename V, typename E>
-typename Graph<V,E>::EdgeIterator Graph<V,E>::endEdge(){
+typename Graph<V,E>::EdgeIterator Graph<V,E>::endEdges(){
     return(EdgeIterator(*this, -1, -1));
 }
 
@@ -404,9 +419,9 @@ typename Graph<V,E>::EdgeIterator Graph<V,E>::endEdge(){
 
 template <typename V, typename E>
 Graph<V,E>::VertexIterator::VertexIterator(Graph<V,E> &graph, int current_vertex){
-    if(graph.noVertices() <= 0){
+    if(graph.nrOfVertices() <= 0){
         throw invalid_argument("No existing vertices");
-    } else if(current_vertex < -1 || current_vertex > graph.noVertices()){
+    } else if(current_vertex < -1 || current_vertex > graph.verx.size()){
         throw invalid_argument("Vertex does not exist");
     } else {
         source = graph.verx;
@@ -442,12 +457,17 @@ V& Graph<V,E>::VertexIterator::operator*(){
 }
 
 template <typename V, typename E>
-typename Graph<V,E>::VertexIterator Graph<V,E>::beginVertex(){
+int& Graph<V,E>::VertexIterator::getIndex(){
+    return((source[it]).index);
+}
+
+template <typename V, typename E>
+typename Graph<V,E>::VertexIterator Graph<V,E>::beginVertices(){
     return VertexIterator(*this, 0);
 }
 
 template <typename V, typename E>
-typename Graph<V,E>::VertexIterator Graph<V,E>::endVertex(){
+typename Graph<V,E>::VertexIterator Graph<V,E>::endVertices(){
     return VertexIterator(*this, int(verx.size()));
 }
 
@@ -460,10 +480,12 @@ Graph<V,E>::DFS::DFS(Graph<V,E> &graph, int current_vertex){
     source = &graph;
     if(current_vertex < -1 || current_vertex > int(source->verx.size()))
         throw invalid_argument("Current vertex outside of range");
-    
-    visited.resize(int(source->verx.size()), false);
+
+    visited.resize(graph.verx.size(), false);
     current_vertex_id = current_vertex;
     path.push(current_vertex_id);
+    if(current_vertex_id != -1)
+        visited[current_vertex_id] = true;
 }
 
 template <typename V, typename E>
@@ -473,26 +495,28 @@ bool Graph<V,E>::DFS::operator==(const DFS &dfs) const{
 
 template <typename V, typename E>
 typename Graph<V,E>::DFS& Graph<V,E>::DFS::operator++(){
-    int s = path.top();
-    path.pop();
-    if(!visited[s])
-        visited[s] = true;
-    
-    //The if statement is redundant in a non-reduced matrix
-    for(int k = source->verx.size(); k >= 0; k--){
-        if(source->verx[k].index != -1){
+    if(!path.empty()){
+        int s = path.top();
+        path.pop();
+
+        if(!visited[s])   
+            visited[s] = true;  
+        
+        for(int k = source->verx.size()-1; k >= 0 ; k--){
             if(source->dirmat[s][k].exists){
-                if(!visited[k])
+                if(!visited[k]){
                     path.push(k);
+                    //cout << "bump" << endl;
+                }
             }
         }
+        
+        if(!path.empty() && !visited[path.top()])
+            current_vertex_id = path.top();
+        else
+            current_vertex_id = -1;
     }
-    if(path.empty()){
-        current_vertex_id = -1;
-        return(*this);
-    }
-    current_vertex_id = path.top();
-    return(*this);
+    return (*this);
 }
 
 template <typename V, typename E>
@@ -508,8 +532,9 @@ V& Graph<V,E>::DFS::operator*(){
 }
 
 template <typename V, typename E>
-typename Graph<V,E>::DFS Graph<V,E>::beginDFS(){
-    return(DFS(*this, 0));
+typename Graph<V,E>::
+DFS Graph<V,E>::beginDFS(int init){
+    return(DFS(*this, init));
 }
 
 template <typename V, typename E>
@@ -526,7 +551,7 @@ Graph<V,E>::BFS::BFS(Graph<V,E> &graph, int current_vertex){
     source = &graph;
     if(current_vertex < -1 || current_vertex > int(source->verx.size()))
         throw invalid_argument("Current vertex outside of range");
-    
+
     visited.resize(int(source->verx.size()), false);
     current_vertex_id = current_vertex;
     queue.push_back(current_vertex_id);
@@ -544,20 +569,19 @@ typename Graph<V,E>::BFS& Graph<V,E>::BFS::operator++(){
     int s = queue.front();
     queue.pop_front();
     for(int k = 0; k < int(source->verx.size()); k++){
-        if(source->verx[k].index != -1){
-            if(source->dirmat[s][k].exists){
-                if(!visited[k]){
-                    visited[k] = true;
-                    queue.push_back(k);
-                }
+        if(source->dirmat[s][k].exists){
+            if(!visited[k]){
+                visited[k] = true;
+                queue.push_back(k);
             }
         }
     }
-    if(queue.empty()){
+    
+    if(queue.empty())
         current_vertex_id = -1;
-        return(*this);
-    }
-    current_vertex_id = queue.front();
+    else
+        current_vertex_id = queue.front();
+    
     return (*this);
 }
 
@@ -574,8 +598,8 @@ V& Graph<V,E>::BFS::operator*(){
 }
 
 template <typename V, typename E>
-typename Graph<V,E>::BFS Graph<V,E>::beginBFS(){
-    return(BFS(*this, 0));
+typename Graph<V,E>::BFS Graph<V,E>::beginBFS(int init){
+    return(BFS(*this, init));
 }
 
 template <typename V, typename E>
